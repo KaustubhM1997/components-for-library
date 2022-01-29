6d755354a869bfc70f67df4f247e97182e430b08
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2022-01-29 18:38:39 +0530

    components

---
+++ badge-components/app.js
@@ -0,0 +1,19 @@
+
+
+const rightButton = document.querySelector('.btn');
+
+const notification = document.querySelector('.right-btn')
+
+
+function toggleBtn(){
+
+    // let name;
+
+    // name.innerText = "Welcome";
+
+    notification.style.display = "none";
+
+
+}
+
+rightButton.addEventListener("click", toggleBtn)
